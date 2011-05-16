7880dfea91c4178282a961fbe3cf1ee0517c51dd
Maciej <maciej@maciejkowalewski.com>
2011-05-16 22:00:50 +0200

    refactored run script controller to use runners module

--- spec/lib/runners/ruby_runner_spec.rb
+++ spec/lib/runners/ruby_runner_spec.rb
@@ -1,5 +1,4 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
-require "#{::Rails.root}/lib/runners/ruby_runner"
 
 describe Runners::RubyRunner do
   it "should run ruby script" do
