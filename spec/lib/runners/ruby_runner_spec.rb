a27fe151f8879879284059ef85616fe2d3585b46
Maciej <maciej@maciejkowalewski.com>
2011-05-16 21:56:10 +0200

    Spec for simple ruby runner

---
+++ spec/lib/runners/ruby_runner_spec.rb
@@ -0,0 +1,12 @@
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+require "#{::Rails.root}/lib/runners/ruby_runner"
+
+describe Runners::RubyRunner do
+  it "should run ruby script" do
+    runner = Runners::RubyRunner.new
+    output = runner.run("puts 'Hello world'")
+    
+    output.should == "Hello world\n"
+  end
+  
+end
