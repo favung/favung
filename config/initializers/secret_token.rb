fe21e81bded4e9a17e1424de716493e6a521a8f7
Tomasz Zurkowski <doriath88@gmail.com>
2011-05-16 15:55:40 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Favung::Application.config.secret_token = 'e26bf73768726761d7c0e022eaac60c3c6e52c07d86e90a081917d85a356738735615a50752a948f13d2178a05c9ee087f2422f318bd26f47b38ab65ba1b1b47'
