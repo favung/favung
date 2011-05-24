198073c4a62bb2af50cbe39d2b052c7ea9380fc0
Tomasz Zurkowski <doriath88@gmail.com>
2011-05-24 12:54:15 +0200

    Add mongoid + GridFS support

--- agent/agent.rb
+++ agent/agent.rb
@@ -1,8 +1,24 @@
 require 'drb'
 
+
+require 'mongo'
+
+connection = Mongo::Connection.new
+$db = connection.db('favung_development')
+
 class Agent
-  def execute(script)
-    `ruby -e "#{script}"`
+  def execute(gridfs_path)
+    script_file = Mongo::GridFileSystem.new($db).open(gridfs_path, 'r')
+    script = script_file.read
+    script_file.close
+
+    output = `ruby -e "#{script}"`
+
+    output_file = Mongo::GridFileSystem.new($db).open("output.txt", 'w')
+    output_file.write output
+    output_file.close
+
+    "output.txt"
   end
 end
 
--- webfront/app/controllers/run_script_controller.rb
+++ webfront/app/controllers/run_script_controller.rb
@@ -1,12 +1,22 @@
 require 'drb'
+require 'mongo'
 
 class RunScriptController < ApplicationController
   def index
   end
 
   def run
+    gridfs_path = 'solution.txt'
+    gridfs_file = Mongo::GridFileSystem.new(Mongoid.database).open(gridfs_path, 'w')
+    gridfs_file.write params[:script][:script]
+    gridfs_file.close
+
     DRb.start_service
-    agent = DRbObject.new nil, params[:script][:service_url]
-    @output = agent.execute(params[:script][:script])
+    agent = DRbObject.new nil, params[:script][:service_url].chomp
+    output_path = agent.execute('solution.txt')
+
+    output_file = Mongo::GridFileSystem.new(Mongoid.database).open(output_path, 'r')
+    @output = output_file.read
+    output_file.close
   end
 end
